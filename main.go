5e51f5810e2cc9edc3e536cd286b1c1586a447ec
pnaumov <pavel.naumov@instamart.ru>
2020-11-29 15:44:58 +0400

    read env variables and create telegram bot client

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+const BotToken = "BOT_TOKEN"
 
 func main() {
+	err := godotenv.Load(".env")
 	c := make(chan string, 1)
 
-	c <- "kek"
+	if err != nil {
+		log.Fatal("oh my god")
+	}
+
+	botToken := os.Getenv(BotToken)
+
+	_, err = tgbotapi.NewBotAPI(botToken)
+
+	if err != nil {
+		log.Fatal("oh my god")
+	}
+
+	c <- botToken
 
 	for {
 		go start(c)
